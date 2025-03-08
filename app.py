9177a7db23c47e68deace37afa582dd6d24edd1b
Shaik Hidaythulla <shaikhidaythulla07@gmail.com>
2025-03-08 12:57:38 +0530

    model traiend and saved

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from src.mlproject.logger import logger
 from src.mlproject.exception import CustomException
 from src.mlproject.components.data_ingestion import DataIngestion
 from src.mlproject.components.data_transformation import DataTransformation
+from src.mlproject.components.model_trainer import ModelTrainer
 
 import sys
 
@@ -14,7 +15,10 @@ if __name__ == "__main__":
         train_data_path, test_data_path=data_ingestion.initiate_data_ingestion()  # ✅ Updated function name
         
         data_transformation = DataTransformation()
-        data_transformation.initiate_data_transformation(train_data_path, test_data_path)
+        train_arr, test_arr, preprocessor_path = data_transformation.initiate_data_transformation(train_data_path, test_data_path)
+        
+        model_trainer = ModelTrainer()
+        print(model_trainer.initiate_model_trainer(train_arr, test_arr))
 
     except Exception as e:
         logger.error("Custom Exception raised")
